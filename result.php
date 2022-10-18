4c7d8702cc73f7b9333fdd041e609aab6b052ae4
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2022-10-18 08:23:24 -0300

    Update result.php

---
+++ result.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="zodiaco.png" type="image/x-icon">
+    <title>Signos</title>
+</head>
+<body>
+    <header>
+        <h1><a href="index.php">Signos</a></h1>
+    </header>
+    <div class="main">
+        <div class="texto">
+            <h2 class="signo">Capricórnio</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam ea nemo assumenda porro temporibus modi corrupti delectus, laboriosam blanditiis! Ullam magni nam quam sit. Ullam corrupti cum illo odit labore!</p>
+        </div>
+        <div class="imagem">
+            <img src="https://cdn.brasildefato.com.br/media/600f7fdf241a4327a8cc30bc054dd85a.jpg"></img>
+        </div>
+    
+    
+    </div>
+</body>
+</html>
