268b2c794c91271e7d2e7a3e2a0e4b4d6e2a398c
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2022-10-18 08:21:48 -0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="zodiaco.png" type="image/x-icon">
+    <title>Signos</title>
+</head>
+<body>
+    <header>
+        <h1>Signos</h1>
+    </header>
+    <div class="main">
+        <div class="texto"><p>Descubra já as características de seu signo a partir de sua data de nascimento:</p>
+        <input type="date" name="date" id="date">
+        <button><a href="result.php">Enviar</a></button><br>
+    </div>
+    <div class="imagem">
+        <img src="https://i0.wp.com/portaledicase.com/wp-content/uploads/2022/09/astrologia-2.jpg"><img>
+    </div>
+<?php
+
+?>
+    </div>
+</body>
+</html>
